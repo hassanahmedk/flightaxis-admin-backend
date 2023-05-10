00e48b3800b684f554c997525935b643ff1fa188
Hassan <mr.und3ad1@gmail.com>
2023-05-10 12:29:57 +0500

    organized code in index.js

--- index.js
+++ index.js
@@ -13,13 +13,16 @@ import messageRoutes from "./routes/messages.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+//  Configuring .env
 dotenv.config();
 
+// Configuring boyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
+// Configuring CORS
+app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -29,6 +32,7 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Connecting database
 const mongodbURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority&ssl=true`;
 
 mongoose.connect(mongodbURL, {
@@ -42,16 +46,19 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
+// Test route
 app.get("/", (req, res) => {
-  res.send("Working");
+  res.send({status:200, message:"API is working"});
 });
 
+// Adding routes
 app.use("/login", loginRoutes);
 app.use("/flights", flightRoutes);
 app.use("/bookings", bookingRoutes);
 app.use("/quotes", quoteRoutes);
 app.use("/messages", messageRoutes);
 
+// Starting server
 app.listen(port, "0.0.0.0", function () {
   console.log("Server running at 5000");
 });
